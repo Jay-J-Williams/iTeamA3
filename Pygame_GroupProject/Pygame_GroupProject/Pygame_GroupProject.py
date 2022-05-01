cf277677b519746e507c82171fe8268d4cf2711f
Adz946 <Adsaleh946@gmail.com>
2022-05-01 14:43:23 +1000

    Deleted my trial code

--- Pygame_GroupProject/Pygame_GroupProject/Pygame_GroupProject.py
+++
@@ -1,4 +0,0 @@
-#Tested the use of a python file in both Visual Studio and VS Code
-print("testing")
-print("Test: 2")
-#Virdict - Both can be used at the same time as a change to one is updated to the other instantly
