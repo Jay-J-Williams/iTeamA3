94cc78b9b63c5c25149a6746f9f9fdac29f647ec
Adz946 <Adsaleh946@gmail.com>
2022-05-01 15:41:49 +1000

    Update Pygame_GroupProject.py

--- Pygame_GroupProject/Pygame_GroupProject/Pygame_GroupProject.py
+++ Pygame_GroupProject/Pygame_GroupProject/Pygame_GroupProject.py
@@ -11,12 +11,12 @@ def display_message(message, number_of_times):
         sys.stdout.write(message + " | " + str((i + 1)) + "\n")
         i += 1
 def get_message():
-    sys.stdout.write("What would you like to display?: ")
+    sys.stdout.write("What would you like to display?\n")
     message = sys.stdin.readline().strip()
     return message
 
 def get_message_amount():
-    sys.stdout.write("Enter the amount of times you want your message to display: ")
+    sys.stdout.write("Enter the amount of times you want your message to display\n")
     amount = int(sys.stdin.readline())
     return amount
 
@@ -38,9 +38,5 @@ def darkest_secret_manager():
     secret = get_darkest_secret()
     display_secret(secret)
 
-    #I don't think secret is spelled right, looks off. Could just be me
-    #I think it's fine. I do that sometimes too.
 darkest_secret_manager()
 message_manager()
-#Stop writing until we get James in here
-#✔
