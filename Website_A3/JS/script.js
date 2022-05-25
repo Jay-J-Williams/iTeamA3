629c37cee83a7341683f63353e7fb8f6a61c392c
Adz946 <100402347+Adz946@users.noreply.github.com>
2022-05-25 14:22:10 +1000

    Started building the A3 website
    Added in all necessary images
    
    Added script file
    
    Added style file
    
    Made an index
    
    Added in the site changes Thomas wanted
    
    All work is in a great folder structure

---
+++ Website_A3/JS/script.js
@@ -0,0 +1,24 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+$(document).ready(function(){
+  $("#B1").click(function(){
+      $("#Adam").slideToggle("slow");
+  });
+  $("#B2").click(function(){
+      $("#Daryl").slideToggle("slow");
+  });
+  $("#B3").click(function(){
+      $("#Jay").slideToggle("slow");
+  });
+  $("#B4").click(function(){
+      $("#James").slideToggle("slow");
+  });
+});
